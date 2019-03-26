1f291268c89655aaa0f702bfe73650b9f7862e8e
Jesper54 <48125883+Jesper54@users.noreply.github.com>
2019-03-26 10:15:40 +0100

    les07 commit

--- src/App.js
+++ src/App.js
@@ -75,21 +75,46 @@ class App extends Component {
     cart: []
   }
 
+  addToCart = (id) => {
+    const gameToAdd = this.state.games.find((game) => {
+      return id === game.id
+    })
+
+    const cart_game = {
+      ...gameToAdd,
+      amount: 1
+    }
+
+    const old_cart = this.state.cart;
+    old_cart.push(cart_game);
+
+    this.setState({
+      cart: old_cart
+    });
+  }
+
+  cartCount = () => {
+    return this.state.cart.length
+  }
+
   render() {
     return (
       <div className="App">
-      <BrowserRouter>
-        <>
-          <Header />
+        <BrowserRouter>
+          <>
+            <Header count={this.cartCount()}/>
             <Route 
               exact path='/'
-              render={() => <Shop games={this.state.games} />}
-              />
-              <Route 
-                path='/cart'
-                render={() => <Cart />}
-              />
-          <Footer />
+              render={() => <Shop
+                games={this.state.games}
+                addToShoppingCart={this.addToCart}
+              />}
+            />
+            <Route
+              path='/cart'
+              render={() => <Cart />}
+            />
+            <Footer />
           </>
         </BrowserRouter>
       </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
 
-class Footer extends Component
+function Footer(props)
 {
-    render() {
-        return (
-            <footer className="text-muted">
-                <div className="container">
-                    <p className="float-right">
-                        &copy; 2019 by The Sick Dude
-                    </p>
-                    <p>Een Game Shop, een voorbeeld app voor de lessen WebApps</p>
-                </div>
-            </footer>
-        )
-    }
+    return (
+        <footer className="text-muted">
+            <div className="container">
+                <p className="float-right">
+                    &copy; 2019 by The Sick Dude
+                </p>
+                <p>Een Game Shop, een voorbeeld app voor de lessen WebApps</p>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
